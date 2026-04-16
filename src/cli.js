3dcc6f43e97ca6461f27e02afbc2df34cc8edb70
ilyalvov <ilyalvov2007@gmail.com>
2026-04-16 10:58:18 +0300

    t758435639485349857

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync'
 
 export const getName = () => {
-  const us =  readlineSync.question('May I have your name? ')
-  console.log(`Hello, ${us}`)
+  return readlineSync.question('May I have your name? ')
 }
